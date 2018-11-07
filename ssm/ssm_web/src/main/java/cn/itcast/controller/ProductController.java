e7b563f40081d6915d20ee69dcf2cd8fab11f586
guanxu <guanxu2018@gmail>
2018-11-07 16:40:57 +0800

    1.2

---
+++ ssm/ssm_web/src/main/java/cn/itcast/controller/ProductController.java
@@ -0,0 +1,27 @@
+package cn.itcast.controller;
+
+import cn.itcast.service.ProductService;
+import cn.itcast.domain.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("product")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @RequestMapping("/findAll")
+    public ModelAndView findAll(){
+        ModelAndView mv=new ModelAndView();
+        List<Product> productList = productService.findAll();
+        mv.addObject("productList",productList);
+        mv.setViewName("product-list");
+        return mv;
+    }
+}
